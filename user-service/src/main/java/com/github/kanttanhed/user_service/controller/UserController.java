a8b10a458ef688df7b02e75a9d303aa0f2393982
Plinio <plinio.salazar@yahoo.com.br>
2025-05-11 23:51:15 +0100

    ✨ feat: Creating Controller

---
+++ user-service/src/main/java/com/github/kanttanhed/user_service/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.github.kanttanhed.user_service.controller;
+
+import com.github.kanttanhed.user_service.dto.UserResponseDTO;
+import com.github.kanttanhed.user_service.service.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserResponseDTO>> getAllUsers() {
+        List<UserResponseDTO> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> getUserById(@PathVariable Long id) {
+        UserResponseDTO user = userService.getUserById(id);
+        return user != null ? ResponseEntity.ok(user) : ResponseEntity.notFound().build();
+    }
+}
