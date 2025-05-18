664adcc049e45d9d9aefcb041e6f4168601d4999
Plinio <plinio.salazar@yahoo.com.br>
2025-05-18 23:59:42 +0100

    ✨ feat: adding Log with SLF4J

--- user-service/src/main/java/com/github/kanttanhed/user_service/service/UserService.java
+++ user-service/src/main/java/com/github/kanttanhed/user_service/service/UserService.java
@@ -4,6 +4,8 @@ import com.github.kanttanhed.user_service.dto.UserRequestDTO;
 import com.github.kanttanhed.user_service.dto.UserResponseDTO;
 import com.github.kanttanhed.user_service.entity.User;
 import com.github.kanttanhed.user_service.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -11,6 +13,8 @@ import java.util.stream.Collectors;
 
 @Service
 public class UserService {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     private final UserRepository userRepository;
     private final UserProducer userProducer;
@@ -22,6 +26,7 @@ public class UserService {
 
     // Method to create a new user
     public UserResponseDTO createUser(UserRequestDTO userRequestDTO) {
+        logger.info("Starting user creation with email: {}", userRequestDTO.getEmail());
 
         // Validate the request
         if (userRequestDTO.getName() == null || userRequestDTO.getEmail() == null) {
@@ -37,12 +42,15 @@ public class UserService {
         // Send the user to the Kafka topic
         userProducer.send(savedUser);
 
+        logger.info("User created successfully. ID: {}", savedUser.getId());
+
         // Return the saved user as a response DTO
         return new UserResponseDTO(savedUser.getId(), savedUser.getName(), savedUser.getEmail());
     }
 
     // Method to get all users
     public List<UserResponseDTO> getAllUsers() {
+        logger.info("Searching for all users");
         List<User> users = userRepository.findAll();
         return users.stream()
                 .map(user -> new UserResponseDTO(user.getId(), user.getName(), user.getEmail()))
@@ -51,6 +59,7 @@ public class UserService {
 
     // Method to get a user by ID
     public UserResponseDTO getUserById(Long id) {
+        logger.info("Searching user by ID: {}", id);
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("User not found"));
         return new UserResponseDTO(user.getId(), user.getName(), user.getEmail());
@@ -58,6 +67,7 @@ public class UserService {
 
     // Method to update a user
     public UserResponseDTO updateUser(Long id, UserRequestDTO userRequestDTO) {
+        logger.info("Updating user with ID: {}", id);
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("User not found"));
 
